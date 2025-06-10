c11b49681c240e8af8bedefd6f18d49a937aff0c
suraj <surajrayudu287@gmail.com>
2025-06-10 15:04:04 -0500

    stage_01_data_ingestion

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Kidney-Disease-classification
+# Workflow
+Update config.yaml
+Update params.yaml
+Update the entity
+Update the configuration manager in src config
+Update the components
+Update the pipeline
+Update the main.py
+Update the dvc.yaml
+app.py
--- main.py
+++ main.py
@@ -1,13 +1,3 @@
 from src.CnnClassifier import logger
-from src.CnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-
-STAGE_NAME = "Data Ingestion stage"
-try:
-   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-   data_ingestion = DataIngestionTrainingPipeline()
-   data_ingestion.main()
-   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-except Exception as e:
-        logger.exception(e)
-        raise e
+logger.info("welcome to custom log")
